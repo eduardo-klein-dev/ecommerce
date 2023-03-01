a3c8ea5cc15728d281228debc65b8036446a349b
eduardo-klein-dev <eduardokleindeveloper@gmail.com>
2023-03-01 10:27:41 -0300

    Quarto Commit: Criação Header

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width='device-width', initial-scale=1.0">
+    <title>Ecommerce</title>
+
+    <!-- Links CSS -->
+    <link rel="stylesheet" href="public/styles/estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <header>
+        <div class="container header">
+            <div class="header-logo">
+                <div class="imagem-logo">
+                    <img src="public/imgs/logo.png" alt="">
+                </div>
+            </div>
+            <nav class="header-menu">
+                <div class="header-menu-search">
+                    <div class="header-menu-search-input">
+                        <input type="text" name="loupe" class="form-control" placeholder="Digite o que procura">
+                    </div>
+                    <div class="header-menu-search-imagem">
+                        <a href="index.php"><img src="public/imgs/loupe.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="header-menu-buttons">
+                    <ul>
+                        <li>
+                            <button type="button" class="active btn btn-light">Início</button>
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-light">Produtos</button>
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-light">Quem somos</button>
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-light">Contatos</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <!-- Links JavaScript: -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" defer integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
+    <script src="public/js/scripts.js" defer></script>
+
+</body>
+
+</html>
